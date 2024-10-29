3e0ae989c57e9e5339d2a306dbbb16efe79e5195
Mikuto <mikuto1227k@gmail.com>
2024-10-29 19:03:00 +0900

    feat:add ScoreLogDAO

--- flying_kokaton.py
+++ flying_kokaton.py
@@ -1,3 +1,4 @@
+import datetime
 import os
 from random import randint as ran
 import sys
@@ -11,21 +12,95 @@ HEIGHT = 650
 WIDTH = 450
 
 # クラス宣言部
+class ScoreLogDAO:
+    """
+    それぞれのスコアデータの入出力を管理するクラス
+    TODO: できればデータベース化
+    """
+    def __init__(self,log_file_name:str = "score_log.csv", file_encoding:str = "utf-8") -> None:
+        """
+        スコアログを保存する先がなければ作成する
+        また、任意のファイル名へと変更できる
+        保存先のパスは"./logs"内になる
+
+        :param str log_file_name: ファイル名
+        :param str file_encoding: ファイルのエンコード方式
+        """
+        self.file_encoding = file_encoding
+        self.log_file_path = "./logs"
+
+        if not os.path.exists(self.log_file_path):
+            # フォルダがなければ作成する
+            os.mkdir(self.log_file_path)
+        
+        self.log_file = self.log_file_path + "/" + log_file_name
+        if not os.path.exists(self.log_file):
+            # ログファイルがなければ初期化する
+            with open(self.log_file, "w", encoding=self.file_encoding) as f:
+                f.write("uuid,player_name,score,created_time\n")
+
+        # タイムスタンプ準備
+        t_delta = datetime.timedelta(hours=9)
+        jts = datetime.timezone(t_delta, 'JST')
+        self.now = datetime.datetime.now(jts)
+    
+    def insert(self, uuid:str, player_name:str, score:int) -> bool:
+        """
+        プレイログを挿入する
+
+        :param str uuid: UUID
+        :param str player_name: プレイヤー名
+        :param str score:
+        :return: 成功したらTrueを返します
+        :rtype: bool
+        """
+        with open(self.log_file, "a", encoding=self.file_encoding) as f:
+            f.write(f"{uuid},{player_name},{score},{self.now.strftime('%Y/%m/%d %H:%M:%S')}\n")
+
+        return True
+    
+    def get(self)->list[tuple[str,str,int,str]]:
+        """
+        保存されているプレイログデータを取得します
+
+        :return: ログデータのtupleを返します
+        :rtype: tuple[tuple[str,str,int,str]]
+        """
+        result = []
+        with open(self.log_file, "r", encoding=self.file_encoding) as f:
+            f.readline()
+            result += [self.dismantling(row) for row in f]
+        
+        return result
+
+    def dismantling(self,row:str)->tuple[str,str,str,str]:
+        """
+        プレイログデータの一行をそれぞれの要素に分解し、tupleで返します
+
+        :return: uuid, player_name, score, created_time
+        :rtype: tuple[str,str,str,str]
+        """
+        datas = row.rstrip("\n").rsplit(",")
+
+        return tuple(datas)
+
 class Score:
     """
     スコア管理システム
     """
-    def __init__(self, player_name:str = "guest"):
+    def __init__(self, session:ScoreLogDAO, player_name:str = "guest"):
         """
         スコアをユーザと紐づけます
         担当 : c0a23019
         
         :param str player_name: プレイヤー名
         """
+        self.session = session
+
         # スコア情報系
         self.value = 0
         self.player_name = player_name
-        self.player_uuid = uuid.uuid1()
+        self.player_uuid = str(uuid.uuid1())
         # TODO: 遊んだ時間のlog取得
 
         # 表示系
@@ -52,9 +127,9 @@ class Score:
         """
         self.value += add_score
 
-    def __delattr__(self) -> None:
+    def save(self) -> None:
         # TODO: クラス削除時にスコアをファイルに保存する
-        pass
+        self.session.insert(self.player_uuid, self.player_name, self.value)
 
 class PuzzleList():
     """
@@ -97,7 +172,8 @@ def main():
     kk_rct.center = 300,200
     # ここまで
 
-    score = Score()
+    score_log_DAO = ScoreLogDAO()
+    
 
     tmr = 0 # 時間保存
 
@@ -127,9 +203,17 @@ def main():
                     if event.type == pg.KEYDOWN:
                         status = "game:0"
                         break
-            case "game:0":                                 
+
+            case "game:0":
+                score = Score(score_log_DAO)
+                status = "game:1"
+
+            case "game:1":                                 
                 for event in pg.event.get():
-                    if event.type == pg.QUIT: return
+                    if event.type == pg.QUIT:
+                        score.save()
+                        print(score_log_DAO.get())
+                        return
 
                 key_lst = pg.key.get_pressed() # 練習8-3 全キーの押下状態取得
                 
@@ -148,6 +232,7 @@ def main():
                 
                 screen.blit(kk_img, kk_rct)
 
+                score.add(10)
                 score.update(screen)
 
         # 共通処理部
