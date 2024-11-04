0b2bdf354750acdb5696bec24c3bc81bb39b7d2c
seo <c0a230874c@edu.teu.ac.jp>
2024-11-04 14:26:29 +0900

    1部が重なり合ったコンボに反応できるように改良

--- flying_kokaton.py
+++ flying_kokaton.py
@@ -1,6 +1,7 @@
 import os
 from random import randint as ran
 import sys
+import time
 import uuid
 import pygame as pg
 
@@ -63,6 +64,8 @@ class PuzzleList:
     """
     パズル画面を管理するリストに関係するクラス
     担当:瀬尾
+    get_lis():生成した盤面を取得する
+    selt_lis(list[list]):盤面をlist[list]に置き換える
     """
 
     def __init__(self):
@@ -87,20 +90,37 @@ class Combo:
         2:縦
     担当:瀬尾
     """
-    combo_lis = []
-    combo_count = 0
+    
+    combo_all = 0
 
     def __init__(self, lis:list[list]):
+        self.combo_count = 0
         self.lis = lis
-        co = True
         check = 0
-        while co:
+        while True:
+            self.cross_combo()
             self.row_combo()
             self.column_combo()
             if check == self.combo_count:
-                co = False
+                break
             check = self.combo_count
-            
+        self.elise(self.lis)
+    
+    def cross_combo(self):
+        for i in range(1, 5):
+            stack = 0
+            for j in range(4):
+                if stack > 0:
+                    stack -= 1
+                    continue
+                if jadge_double(self.lis[i][j], self.lis[i][j+1], self.lis[i][j+2], self.lis[i-1][j+1], self.lis[i+1][j+1]) and jadge_combo(self.lis[i][j], self.lis[i][j+1]) and jadge_combo(self.lis[i][j], self.lis[i][j+2]) and jadge_combo(self.lis[i][j], self.lis[i-1][j+1]) and jadge_combo(self.lis[i][j], self.lis[i+1][j+1]):
+                    stack += 2
+                    print("cross", [i, j])
+                    self.combo_add()
+                    self.change(3, i, j, ele = self.lis[i][j])
+                    time.sleep(0.5)
+                    break
+
     def row_combo(self):
        for i in range(6):
             stack = 0
@@ -109,21 +129,21 @@ class Combo:
                     stack -= 1
                     continue
                 combo_len = 3
-                if self.lis[i][j] == self.lis[i][j+1] and self.lis[i][j] == self.lis[i][j+2] and self.lis[i][j] != 0:
-                    if j <= 2 and self.lis[i][j] == self.lis[i][j+3]:
+                if jadge_double(self.lis[i][j], self.lis[i][j+1], self.lis[i][j+2]) and jadge_combo(self.lis[i][j], self.lis[i][j+1]) and jadge_combo(self.lis[i][j], self.lis[i][j+2]):
+                    if j <= 2 and jadge_combo(self.lis[i][j], self.lis[i][j+3]):
                         combo_len += 1
-                        if j <= 1 and self.lis[i][j] == self.lis[i][j+4]:
+                        if j <= 1 and jadge_combo(self.lis[i][j], self.lis[i][j+4]):
                             combo_len += 1
-                            if j <= 0 and self.lis[i][j] == self.lis[i][j+5]:
+                            if j <= 0 and jadge_combo(self.lis[i][j], self.lis[i][j+5]):
                                 combo_len += 1
-                    set = [1, i, j, combo_len]
                     self.combo_count += 1
                     stack += combo_len-1
-                    print("j", [i, j], combo_len)
-                    self.elise(set)
+                    print("row", [i, j], combo_len)
+                    self.combo_add()
+                    self.change(1, i, j, self.lis[i][j],combo_len)
+                    time.sleep(0.5)
                     break
                     
-    
     def column_combo(self):
         for j in range(6):
             stack = 0
@@ -132,18 +152,19 @@ class Combo:
                     stack -= 1
                     continue
                 combo_len = 3
-                if self.lis[i][j] == self.lis[i+1][j] and self.lis[i][j] == self.lis[i+2][j] and self.lis[i][j] != 0:
-                    if i <= 2 and self.lis[i][j] == self.lis[i+3][j]:
+                if jadge_double(self.lis[i][j], self.lis[i+1][j], self.lis[i+2][j]) and jadge_combo(self.lis[i][j], self.lis[i+1][j]) and jadge_combo(self.lis[i][j], self.lis[i+2][j]):
+                    if i <= 2 and jadge_combo(self.lis[i][j], self.lis[i+3][j]):
                         combo_len += 1
                         if i <= 1 and self.lis[i][j] == self.lis[i+4][j]:
                             combo_len += 1
-                            if i <= 0 and self.lis[i][j] == self.lis[i+5][j]:
+                            if i <= 0 and jadge_combo(self.lis[i][j], self.lis[i+5][j]):
                                 combo_len += 1
-                    set = [2, i, j, combo_len]
                     self.combo_count += 1
                     stack += combo_len-1
-                    print("i", [i, j], combo_len)
-                    self.elise(set)
+                    print("column", [i, j], combo_len)
+                    self.combo_add()
+                    self.change(2, i, j, self.lis[i][j],combo_len)
+                    time.sleep(0.5)
                     break
 
     def get_count(self):
@@ -152,19 +173,44 @@ class Combo:
     def get_lis(self):
         return self.lis
     
-    def elise(self, set:list):
+    def change(self, combo_type:int, i:int, j:int, ele:int = 0, combo_len:int = 1):
         #引数:lst　ボールの種類を保持するリスト
         #コンボ判定されたball_lstを0にする
-        combo_type, i, j, combo_len = set
+        if ele >= 10:
+            ele = ele - 10
         if combo_type == 1:
             for n in range(combo_len):
-                lis[i][j+n] = 0
+                lis[i][j+n] = 10+ele
         elif combo_type == 2:
             for n in range(combo_len):
-                lis[i+n][j] = 0
+                lis[i+n][j] = 10+ele
+        elif combo_type == 3:
+            for n in range(3):
+                lis[i][j+n] = 10+ele
+            lis[i-1][j+1] = 10+ele
+            lis[i+1][j+1] = 10+ele
+
         self.lis = lis
 
+    
+    def elise(self, lis:list[list]):
+        for i in range(len(lis)):
+            for j in range(len(lis[i])):
+                if lis[i][j] >= 10:
+                    lis[i][j] = 0
+        self.lis = lis
+    
+    @classmethod
+    def combo_add(self):
+        Combo.combo_all += 1
 
+    @classmethod
+    def get_combo(self):
+        return Combo.combo_all
+    
+    @classmethod
+    def reset(self):
+        Combo.combo_all = 0
 
 # 関数宣言部
 def drop_down(lis:list[list])->list[list]:
@@ -185,26 +231,36 @@ def drop_down(lis:list[list])->list[list]:
                 jadge[i] = 1
         if all(jadge):
             check = False
+    time.sleep(1)
     return lis
 
 
+def jadge_combo(a, b):
+    if a == b or a + 10 == b or a == b + 10:
+        return True
+    else:
+        return False
+
+def jadge_double(a:int = 10, b:int = 10, c:int = 10, d:int = 10, e:int = 10, f:int = 10, g:int = 10, h:int = 10, i:int = 10):
+    if a >= 10 and b >= 10 and c >= 10 and d >= 10 and e >= 10 and f >= 10 and g >= 10 and h >= 10 and i >= 10:
+        return False
+    else:
+        return True
 
 # 実行確認用
-# lis = [[1, 1, 1, 2, 4, 2], 
-#        [2, 2, 2, 3, 4, 4], 
-#        [3, 2, 1, 2, 4, 5], 
-#        [4, 4, 2, 5, 4, 5], 
-#        [3, 1, 3, 4, 4, 5], 
-#        [3, 3, 3, 2, 4, 1]]
-lis_m = PuzzleList()
-lis = lis_m.get_lis()
+lis = [[1, 1, 1, 2, 4, 2], 
+       [2, 2, 2, 3, 4, 4], 
+       [3, 2, 2, 2, 4, 5], 
+       [4, 4, 2, 5, 4, 5], 
+       [3, 1, 3, 4, 4, 5], 
+       [3, 3, 3, 2, 4, 1]]
+# lis_m = PuzzleList()
+# lis = lis_m.get_lis()
 print(lis[0], "\n", lis[1], "\n", lis[2], "\n", lis[3], "\n", lis[4], "\n", lis[5], "\n")
-count = 0
 while True:
     check = Combo(lis)
     co = check.get_count()
     if co <= 0:
-        print(co)
         break
     lis = check.get_lis()
     print(lis[0], "\n", lis[1], "\n", lis[2], "\n", lis[3], "\n", lis[4], "\n", lis[5])
@@ -212,8 +268,8 @@ while True:
     lis = drop_down(lis)
     print()
     print(lis[0], "\n", lis[1], "\n", lis[2], "\n", lis[3], "\n", lis[4], "\n", lis[5])
-    count += 1
-    print("count", count, "co", co)
+    print("combo",Combo.get_combo(), "co", co)
+Combo.reset()
 
 # # main関数
 # def main():
